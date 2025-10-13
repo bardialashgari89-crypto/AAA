748804bfe9b26150609608b6427a26ebaebb0234
Bardia <bardialashgari89@gmail.com>
2025-10-13 13:02:45 +0330

    first

---
+++ project.py
@@ -0,0 +1,14 @@
+# my_list=[1,2,3,4,5]
+# number =[6]
+# my_list = my_list + number
+# print(my_list)
+
+
+# my_list = [11,13,33,55,39]
+# my_list.insert(len(my_list),73)
+# print(my_list)
+
+
+
+
+
