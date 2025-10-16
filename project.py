f8ab9c22ff501a0124031f89d53a91df10c5d859
Bardia <bardialashgari89@gmail.com>
2025-10-16 17:19:16 +0330

    second commit

--- project.py
+++ project.py
@@ -1,15 +1,4 @@
-# my_list=[1,2,3,4,5]
-# number =[6]
-# my_list = my_list + number
-# print(my_list)
-
-
-# my_list = [11,13,33,55,39]
-# my_list.insert(len(my_list),73)
-# print(my_list)
-
-print("hello-world")
-print(87)
-
-
-print("hi sexi")
+My_List = [1,2,3,4,5]
+number=6
+My_List.append(number)
+print(My_List)
