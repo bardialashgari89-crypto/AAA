c9307f22a487cd2a82a9f56ab4262e31560dca4f
Bardia <bardialashgari89@gmail.com>
2025-10-16 19:39:00 +0330

    project 1

--- project.py
+++ project.py
@@ -1,5 +1,37 @@
-# for
-my_list = [1,2,3,4,5]
-for i in (7,8,9,10):
-    my_list.append(i)
-    print(my_list)
+# project 1
+
+
+import datetime
+
+
+def make_backup(data):
+     backup_data=data.copy()
+     backup_time=datetime.datetime.now().strftime("%Y_%m_%d %H:%M:%S")
+     return backup_data,backup_time
+
+
+
+
+grades=[12,13,3,17,13.25,14,20,20,19]
+print("20 repeted many times? :",grades.count(20))
+print("20 repeted many times? :",grades.count(13))
+print("20 repeted many times? :",grades.count(0))
+
+
+
+grades.sort()
+print("Ascending list grades : ",grades)
+
+
+
+grades.sort(reverse=True)
+print("descending list grades :",grades)
+
+
+
+grades.append(13.13)
+backup = make_backup(grades)
+grades.append(17.17)
+backup =make_backup(grades)
+print("main list : ",grades)        
+print("back up list :",backup)
